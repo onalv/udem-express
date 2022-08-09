005be48398f0d77cad9410ba935428c98380e8c1
Omar Nava <omar@bearing.ai>
2022-08-08 20:58:06 -0500

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /', () => {
+  it('responds with Express', (done) => {
+    request(app).get('/').expect('Express', done);
+  });
+});
